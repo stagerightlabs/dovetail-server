2328b538336573a30476ae3dc9859e9a2ac6e92e
Ryan Durham <ryan@stagerightlabs.com>
2018-09-09 14:53:47 -0700

    Add rank labels to access levels

--- app/AccessLevel.php
+++ app/AccessLevel.php
@@ -4,9 +4,36 @@ namespace App;
 
 class AccessLevel
 {
+    /**
+     * Access Level Tiers
+     */
     public static $SUPER_ADMIN = 1000;
-
     public static $ORGANIZATION_ADMIN = 800;
+    public static $ORGANIZATION_MEMBER = 600;
 
-    public static $ORGANIZATION_USER = 600;
+    /**
+     * Convert an access level into a representational string
+     *
+     * @param int $level
+     * @return string
+     */
+    public static function rank($level)
+    {
+        switch ($level) {
+            case self::$SUPER_ADMIN:
+                return 'Super Admin';
+                break;
+
+            case self::$ORGANIZATION_ADMIN:
+                return 'Admin';
+                break;
+
+            case self::$ORGANIZATION_MEMBER:
+                return 'Member';
+
+            default:
+                return 'None';
+                break;
+        }
+    }
 }
