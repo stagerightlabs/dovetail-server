14aa4cc3e67b60e18b83f9021256a0f08f57e2b5
Ryan Durham <ryan@stagerightlabs.com>
2018-11-26 15:14:22 -0800

    Ignore sentry error reporting locally and when testing

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -34,7 +34,7 @@ class Handler extends ExceptionHandler
      */
     public function report(Exception $exception)
     {
-        if (app()->bound('sentry') && $this->shouldReport($exception)) {
+        if (app()->bound('sentry') && $this->shouldReport($exception) && app()->environment(['production', 'staging'])) {
             app('sentry')->captureException($exception);
         }
 
