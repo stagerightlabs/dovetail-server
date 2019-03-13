48d6efb9188a17dced681a896fe0a606d1fd3ea8
Laravel Shift <shift@laravelshift.com>
2019-03-13 04:13:37 +0000

    Leverage command autoloading
    Laravel [automatically loads Commands][1] within the `app/Console/Command` folder. As such, there is no need to register them within the _Console Kernel_.
    
    [1]: https://laravel.com/docs/5.7/artisan#registering-commands

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,8 +15,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        CrudGenerator::class,
-        CreateSuperAdmin::class,
+        
     ];
 
     /**
